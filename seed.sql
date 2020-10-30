155676788d6a16e9398c64512d29d495af9efe97
Sean Kempf <poserposer@me.com>
2020-10-29 18:26:32 -0700

    Created the seed.sql file and the server file to connect to MySQL

---
+++ seed.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_trackerdb;
+
+CREATE DATABASE employee_trackerdb;
+
+USE employee_trackerdb;
+
+CREATE TABLE employee(
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT
+);
+
+CREATE TABLE role(
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT
+);
+
+CREATE TABLE department (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30)
+);
